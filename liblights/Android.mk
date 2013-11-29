d070305c904dd479d5d451fbb4c3e5527c60d4a6
Brandon McAnsh <brandon.mcansh@gmail.com>
2013-11-29 07:06:13 -0600

    HALs: Remove uneeded barriers
    * These are verfied in device Android.mk
    * No need to double verify
    
    Change-Id: If6573869c3a057cc9128da53652ab59e469b5275
    Signed-off-by: Brandon McAnsh <brandon.mcansh@gmail.com>

--- liblights/Android.mk
+++ liblights/Android.mk
@@ -14,10 +14,6 @@
 
 LOCAL_PATH:= $(call my-dir)
 
-ifeq ($(TARGET_PROVIDES_LIBLIGHT),true)
-ifeq ($(BOARD_VENDOR),htc)
-ifeq ($(TARGET_BOARD_PLATFORM),msm8960)
-
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := lights.msm8960
@@ -33,6 +29,3 @@ LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
 
-endif # TARGET_BOARD_PLATFORM
-endif # TARGET_VENDOR
-endif # TARGET_PROVIDES_LIBLIGHT
